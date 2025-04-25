1b6c76bc72d067613a94676c20511cde8107d870
ThainaraCruz <alvesthainara98@gmail.com>
2025-04-25 13:15:06 -0300

    Categoria Estilizada

--- src/components/categoria/deletarcategoria/DeletarCategoria.tsx
+++ src/components/categoria/deletarcategoria/DeletarCategoria.tsx
@@ -6,107 +6,109 @@ import { RotatingLines } from "react-loader-spinner"
 import { AuthContext } from "../../../context/AuthContext"
 
 function DeletarCategoria() {
+  const navigate = useNavigate()
+  const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const navigate = useNavigate()
+  const { usuario, handleLogout } = useContext(AuthContext)
+  const token = usuario.token
 
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    
-    const { usuario, handleLogout } = useContext(AuthContext)
-    const token = usuario.token
+  const { id } = useParams<{ id: string }>()
 
-    const { id } = useParams<{ id: string }>()
-
-    async function buscarPorId(id: string) {
-        try {
-            await buscar(`/categorias/${id}`, setCategoria, {
-                headers: {
-                    'Authorization': token
-                }
-            })
-        } catch (error: any) {
-            if (error.toString().includes('403')) {
-                handleLogout()
-            }
-        }
+  async function buscarPorId(id: string) {
+    try {
+      await buscar(`/categorias/${id}`, setCategoria, {
+        headers: {
+          Authorization: token,
+        },
+      })
+    } catch (error: any) {
+      if (error.toString().includes("403")) {
+        handleLogout()
+      }
     }
+  }
 
-    useEffect(() => {
-        if (token === '') {
-            alert('Você precisa estar logado')
-            navigate('/')
-        }
-    }, [token])
-
-    useEffect(() => {
-        if (id !== undefined) {
-            buscarPorId(id)
-        }
-    }, [id])
-
-    async function deletarCategoria() {
-        setIsLoading(true)
-
-        try {
-            await deletar(`/categorias/${id}`, {
-                headers: {
-                    'Authorization': token
-                }
-            })
-
-            alert('Categoria apagada com sucesso')
-
-        } catch (error: any) {
-            if (error.toString().includes('403')) {
-                handleLogout()
-            } else {
-                alert('Erro ao deletar a categoria.')
-            }
-        }
-
-        setIsLoading(false)
-        retornar()
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado")
+      navigate("/")
     }
+  }, [token])
 
-    function retornar() {
-        navigate("/categorias")
+  useEffect(() => {
+    if (id !== undefined) {
+      buscarPorId(id)
     }
-    
-    return (
-        <div className='container w-1/3 mx-auto'>
-            <h1 className='text-4xl text-center my-4'>Deletar categoria</h1>
-            <p className='text-center font-semibold mb-4'>
-                Você tem certeza de que deseja apagar a categoria a seguir?</p>
-            <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-                <header 
-                    className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>
-                    Categoria
-                </header>
-                <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.setor}</p>
-                <div className="flex">
-                    <button 
-                        className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2'
-                        onClick={retornar}>
-                        Não
-                    </button>
-                    <button 
-                        className='w-full text-slate-100 bg-indigo-400 
-                                   hover:bg-indigo-600 flex items-center justify-center'
-                                   onClick={deletarCategoria}>
-                        {isLoading ?
-                            <RotatingLines
-                                strokeColor="white"
-                                strokeWidth="5"
-                                animationDuration="0.75"
-                                width="24"
-                                visible={true}
-                            /> :
-                            <span>Sim</span>
-                        }
-                    </button>
-                </div>
-            </div>
+  }, [id])
+
+  async function deletarCategoria() {
+    setIsLoading(true)
+    try {
+      await deletar(`/categorias/${id}`, {
+        headers: { Authorization: token },
+      })
+      alert("Categoria apagada com sucesso")
+    } catch (error: any) {
+      if (error.toString().includes("403")) {
+        handleLogout()
+      } else {
+        alert("Erro ao deletar a categoria.")
+      }
+    }
+    setIsLoading(false)
+    retornar()
+  }
+
+  function retornar() {
+    navigate("/categorias")
+  }
+
+  return (
+    <div className="container w-full md:w-1/3 mx-auto px-4">
+      <h1 className="text-4xl text-center my-6 text-[#1D907D] font-bold">
+        Deletar Categoria
+      </h1>
+      <p className="text-center font-semibold mb-4 text-[#d97667]">
+        Você tem certeza de que deseja apagar a categoria abaixo?
+      </p>
+
+      <div className="border border-[#d0b75c] flex flex-col rounded-2xl overflow-hidden shadow-md">
+        <header className="py-2 px-6 bg-[#f7c98f] text-[#1D907D] font-bold text-2xl text-center">
+          Categoria
+        </header>
+
+        <p className="p-8 text-3xl bg-[#fdf9f6] text-[#d97667] h-full text-center">
+          {categoria.setor}
+        </p>
+
+        <div className="flex">
+          <button
+            className="w-full text-white bg-red-400 hover:bg-red-700 py-2 transition-all"
+            onClick={retornar}
+          >
+            Não
+          </button>
+          <button
+            className="w-full text-white bg-[#1D907D] hover:bg-[#157465] py-2 flex justify-center transition-all"
+            onClick={deletarCategoria}
+          >
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              />
+            ) : (
+              <span>Sim</span>
+            )}
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
+
 export default DeletarCategoria
--- src/components/categoria/formcategoria/FormCategoria.tsx
+++ src/components/categoria/formcategoria/FormCategoria.tsx
@@ -7,128 +7,120 @@ import { AuthContext } from "../../../context/AuthContext";
 import { ToastAlerta } from "../../../utils/ToastAlerta";
 
 function FormCategoria() {
+  const navigate = useNavigate();
+  const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const navigate = useNavigate();
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
+  const { id } = useParams<{ id: string }>();
 
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+  async function buscarPorId(id: string) {
+    try {
+      await buscar(`/categorias/${id}`, setCategoria, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("403")) handleLogout();
+    }
+  }
 
-    const { usuario, handleLogout } = useContext(AuthContext)
-    const token = usuario.token
+  useEffect(() => {
+    if (token === "") {
+      ToastAlerta("Você precisa estar logado!", "info");
+      navigate("/");
+    }
+  }, [token]);
 
-    const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    if (id !== undefined) buscarPorId(id);
+  }, [id]);
 
-    async function buscarPorId(id: string) {
-        try {
-            await buscar(`/categorias/${id}`, setCategoria, {
-                headers: { Authorization: token }
-            })
-        } catch (error: any) {
-            if (error.toString().includes('403')) {
-                handleLogout()
-            }
-        }
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setCategoria({
+      ...categoria,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function retornar() {
+    navigate("/categorias");
+  }
+
+  async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      if (id !== undefined) {
+        await atualizar(`/categorias`, categoria, setCategoria, {
+          headers: { Authorization: token },
+        });
+        ToastAlerta("A Categoria foi atualizada com sucesso!", "sucesso");
+      } else {
+        await cadastrar(`/categorias`, categoria, setCategoria, {
+          headers: { Authorization: token },
+        });
+        ToastAlerta("A Categoria foi cadastrada com sucesso!", "sucesso");
+      }
+    } catch (error: any) {
+      if (error.toString().includes("403")) {
+        handleLogout();
+      } else {
+        ToastAlerta("Erro ao salvar a categoria.", "erro");
+      }
     }
 
-    useEffect(() => {
-        if (token === '') {
-            ToastAlerta('Você precisa estar logado!','info')
-            navigate('/')
-        }
-    }, [token])
+    setIsLoading(false);
+    retornar();
+  }
 
-    useEffect(() => {
-        if (id !== undefined) {
-            buscarPorId(id)
-        }
-    }, [id])
+  return (
+    <div className="container flex flex-col items-center justify-center mx-auto px-4">
+      <h1 className="text-4xl font-bold text-center text-[#1D907D] my-8">
+        {id === undefined ? "Cadastrar Categoria" : "Editar Categoria"}
+      </h1>
 
-    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-        setCategoria({
-            ...categoria,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    function retornar() {
-        navigate("/categorias")
-    }
-
-    async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
-        setIsLoading(true)
-
-        if (id !== undefined) {
-            try {
-                await atualizar(`/categorias`, categoria, setCategoria, {
-                    headers: { 'Authorization': token }
-                })
-                ToastAlerta('A Categoria foi atualizada com sucesso!', 'sucesso')
-            } catch (error: any) {
-                if (error.toString().includes('403')) {
-                    handleLogout();
-                } else {
-                    ToastAlerta('Erro ao atualizar a categoria.', 'erro')
-                }
-
-            }
-        } else {
-            try {
-                await cadastrar(`/categorias`, categoria, setCategoria, {
-                    headers: { 'Authorization': token }
-                })
-                ToastAlerta('A Categoria foi cadastrada com sucesso!', 'sucesso')
-            } catch (error: any) {
-                if (error.toString().includes('403')) {
-                    handleLogout();
-                } else {
-                    ToastAlerta('Erro ao cadastrar a categoria.', 'erro')
-                }
-
-            }
-        }
-
-        setIsLoading(false)
-        retornar()
-    }
-
-    return (
-        <div className="container flex flex-col items-center justify-center mx-auto">
-            <h1 className="text-4xl text-center my-8">
-                {id === undefined ? 'Cadastrar Categoria' : 'Editar Categoria'}
-            </h1>
-
-            <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="descricao">Descrição da Categoria</label>
-                    <input
-                        type="text"
-                        placeholder="Descreva aqui sua categoria"
-                        name='setor'
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={categoria.setor}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <button
-                    className="rounded text-slate-100 bg-indigo-400 
-                               hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
-                    type="submit">
-                    {isLoading ?
-                        <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
-                            visible={true}
-                        /> :
-                        <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
-
-                    }
-                </button>
-            </form>
+      <form
+        className="w-full md:w-1/2 bg-white border border-[#d0b75c] rounded-2xl p-6 shadow-md flex flex-col gap-6"
+        onSubmit={gerarNovaCategoria}
+      >
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="setor"
+            className="text-[#4B2142] font-semibold"
+          >
+            Setor da Categoria
+          </label>
+          <input
+            type="text"
+            placeholder="Descreva aqui sua categoria"
+            name="setor"
+            className="border-2 border-[#f7c98f] rounded p-2 focus:outline-[#1D907D]"
+            value={categoria.setor}
+            onChange={atualizarEstado}
+          />
         </div>
-    );
+
+        <button
+          className="bg-[#1D907D] hover:bg-[#157465] text-white font-semibold rounded py-2 w-1/2 mx-auto flex justify-center transition-all"
+          type="submit"
+        >
+          {isLoading ? (
+            <RotatingLines
+              strokeColor="white"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="24"
+              visible={true}
+            />
+          ) : (
+            <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
+          )}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default FormCategoria;
